f1853010aa5b561c2a0c9667cbc953e0efdd3ce7
Bas van Dijk <v.dijk.bas@gmail.com>
2017-07-25 00:25:46 +0200

    Bump version to 0.5.1.5

--- default.nix
+++ default.nix
@@ -1,2 +1,15 @@
-let pkgs = import <nixpkgs> {};
-in pkgs.haskellPackages.callPackage ./threads.nix {}
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage (import ./threads.nix) {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
